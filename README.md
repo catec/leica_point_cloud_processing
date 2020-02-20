af01b5e82a87861accd2a5eb8e80ba54e38d4789
imlara <imlara@catec.aero>
2020-02-20 09:01:55 +0100

    created new node to process pcl as laser info

--- README.md
+++ README.md
@@ -31,6 +31,46 @@ Meshes and pointclouds are not updated online because they are confidential.
 * Downsample pointcloud     -> reduce the number of points of the pc
 * Apply passthrough filter  -> cut off values that are either inside or outside a given range (not useful for now)
 
+## Dependencies ##
+
+* PCL:
+
+    1. Boost
+
+        sudo apt-get install libboost1.58*
+
+    2. Flann
+
+        sudo apt-get install libflann1.8
+
+    3. Eigen3
+
+        sudo apt-get install libeigen3-dev
+
+    4. VTK
+
+        wget http://www.vtk.org/files/release/7.1/VTK-7.1.0.tar.gz
+        tar -xf VTK-7.1.0.tar.gz
+        cd VTK-7.1.0 && mkdir build && cd build
+        cmake ..
+        make                                                                   
+        sudo make install
+
+    5. PCL y PCL-ROS
+
+        sudo apt-get install ros-kinetic-pcl-*
+
+        wget https://github.com/PointCloudLibrary/pcl/archive/pcl-1.8.0.tar.gz
+        tar -xf pcl-1.8.0.tar.gz
+        cd pcl-pcl-1.8.0 && mkdir build && cd build
+        cmake ..
+        make
+        sudo make install
+
+* PYTHON-PCL
+
+    pip3 install python-pcl
+
 ## Help ##
 Ines M. Lara - imlara@catec.aero
 Other community or team contact
---
+++ src/laserscan_to_pointcloud.cpp
@@ -0,0 +1,56 @@
+#include "ros/ros.h"
+#include <tf/transform_listener.h>
+#include <laser_geometry/laser_geometry.h>
+#include "pcl_conversions/pcl_conversions.h"
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+class My_Filter {
+     public:
+        My_Filter();
+        void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+     private:
+        ros::NodeHandle node_;
+        laser_geometry::LaserProjection projector_;
+        tf::TransformListener tfListener_;
+
+        pcl::PointCloud<pcl::PointXYZ> total_cloud_;
+
+        ros::Publisher point_cloud_publisher_;
+        ros::Subscriber scan_sub_;
+
+        // sensor_msgs::PointCloud2 total_cloud_;
+};
+
+void My_Filter::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
+    sensor_msgs::PointCloud2 cloud,total_cloud_msg;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pcl;
+
+    cloud_pcl.reset(new pcl::PointCloud<pcl::PointXYZ>);
+
+    projector_.transformLaserScanToPointCloud("/world", *scan, cloud, tfListener_);
+    
+    pcl::fromROSMsg(cloud, *cloud_pcl);
+    total_cloud_ += *cloud_pcl;
+    
+    pcl::toROSMsg(total_cloud_,total_cloud_msg);
+    point_cloud_publisher_.publish(total_cloud_msg);
+}
+
+My_Filter::My_Filter(){
+        scan_sub_ = node_.subscribe<sensor_msgs::LaserScan> ("/laser/scan", 100, &My_Filter::scanCallback, this);
+        point_cloud_publisher_ = node_.advertise<sensor_msgs::PointCloud2> ("/cloud", 100, false);
+        // tfListener_.setExtrapolationLimit(ros::Duration(0.1));
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "my_filter");
+    ROS_INFO("tf from laserscan to pointcloud");
+
+    My_Filter filter;
+
+    ros::spin();
+
+    return 0;
+}
