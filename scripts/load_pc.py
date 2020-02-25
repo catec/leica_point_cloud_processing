139aa9dd8eaf14e3e8bda277dd72b03d77da2039
imlara <imlara@catec.aero>
2020-02-25 16:50:24 +0100

    created script to load and publish pc and convert pts to pcd file format

---
+++ scripts/load_pc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+
+import rospy
+import rospkg
+import os
+import pcl
+from pcl import pcl_visualization
+from pcl_helper import *
+
+if __name__ == '__main__':
+    rospy.init_node('load_pointcloud')
+
+    rospack = rospkg.RosPack()
+    pc_path = rospack.get_path("leica_scanstation") + "/pointclouds/"
+    print(pc_path)
+
+    pc_sub = rospy.Publisher("/point_cloud", PointCloud2, queue_size=1)
+
+    # Load point cloud
+    pc = pcl.load_XYZRGB(pc_path + "Prueba1.pcd")
+
+    # print "viewer"
+    # os.system("pcl_viewer " + pc_path + "Prueba1.pcd")
+
+    # Convert to ros
+    ros_pc = pcl_to_ros(pc)
+    
+    while not rospy.is_shutdown():
+        pc_sub.publish(ros_pc)
